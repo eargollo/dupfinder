1113c25810b155c02310a8b2d9b603404aaf22a8
Eduardo Argollo <eargollo@gmail.com>
2023-08-22 20:40:21 -0700

    Cleaning files

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Fast and simple command line tool, with caching capabilities, to find and clean 
 
 ## Usage
 
+1. Search for duplicated files 
+
 Finding duplicated files at a path. Results will be saved on `duplicates.txt`:
 ```
 dupfinder scan /path/to/scan
@@ -19,6 +21,29 @@ for comparison. From second execution on, most file signatures will already be c
 
 The scan shows an execution log. Output file will have the list of duplicates sorted by
 the group size.
+
+2. Edit the output fiile and add a d to the braket of the files you want to delete
+
+Open the outpute text file and you will see the list of duplicated files such as:
+```
+Duplicate 0 Size 732631040 Files 2 MD5 8cb9641aaaaaa0dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[]    '/files/a/filea.avi'
+[]    '/files/b/filea.avi'
+[]    '/files/a/filec.avi'
+```
+
+Add a `[d]` to the files you want to delete, for example:
+```
+Duplicate 0 Size 732631040 Files 2 MD5 8cb9641aaaaaa0dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[]    '/files/a/filea.avi'
+[d]    '/files/b/filea.avi'
+[d]    '/files/a/filec.avi'
+```
+
+Run the clean command on the file:
+```
+dupfinder clean duplicates.txt
+```
 
 ### Managing the cache
 
---
+++ internal/cleaner/cleaner_test.go
@@ -0,0 +1,77 @@
+package cleaner
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestCleaner_load(t *testing.T) {
+	tests := []struct {
+		name    string
+		content string
+		want    []Action
+		wantErr bool
+	}{
+		{name: "Nothing", content: "Nothing here", want: []Action{}, wantErr: false},
+		{
+			name: "Keep files",
+			content: `Found 1 duplicate groups with 2 duplicated files taking 7.4 GiB of extra space:
+Duplicate 0 Size 732631040 Files 2 MD5 8cb9641ad22300dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[]    '/files/a/filea.avi'
+[]    '/files/b/filea.avi'
+[]    '/files/a/filec.avi'
+`,
+			want: []Action{
+				{Group: 0, Filename: "/files/a/filea.avi", Type: Keep, To: ""},
+				{Group: 0, Filename: "/files/b/filea.avi", Type: Keep, To: ""},
+				{Group: 0, Filename: "/files/a/filec.avi", Type: Keep, To: ""},
+			},
+			wantErr: false},
+		{
+			name: "Err missing file",
+			content: `Found 1 duplicate groups with 2 duplicated files taking 7.4 GiB of extra space:
+Duplicate 0 Size 732631040 Files 2 MD5 8cb9641ad22300dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[]    '/files/a/filea.avi'
+[]
+[]    '/files/a/filec.avi'
+`,
+			want: []Action{
+				{Group: 0, Filename: "/files/a/filea.avi", Type: Keep, To: ""},
+			},
+			wantErr: true,
+		},
+		{
+			name: "Keep and delete files",
+			content: `Found 1 duplicate groups with 2 duplicated files taking 7.4 GiB of extra space:
+Duplicate 0 Size 732631040 Files 2 MD5 8cb9641ad22300dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[]    '/files/a/filea.avi'
+[]    '/files/b/filea.avi'
+[]    '/files/a/filec.avi'
+Duplicate 1 Size 9183 Files 4 MD5 8cb9641ad22300dbec61e0299116911a8275018afa35d92e6f213dc40464375b
+[K]    '/files/a/filea.avi'
+[D]    '/files/b/filea.avi'
+[d]    '/files/a/filec.avi'
+`,
+			want: []Action{
+				{Group: 0, Filename: "/files/a/filea.avi", Type: Keep, To: ""},
+				{Group: 0, Filename: "/files/b/filea.avi", Type: Keep, To: ""},
+				{Group: 0, Filename: "/files/a/filec.avi", Type: Keep, To: ""},
+				{Group: 1, Filename: "/files/a/filea.avi", Type: Keep, To: ""},
+				{Group: 1, Filename: "/files/b/filea.avi", Type: Delete, To: ""},
+				{Group: 1, Filename: "/files/a/filec.avi", Type: Delete, To: ""},
+			},
+			wantErr: false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := New()
+			err := c.Load(tt.content)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("err = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(c.actions, tt.want) {
+				t.Errorf("New() = %v, want %v", c.actions, tt.want)
+			}
+		})
+	}
+}
